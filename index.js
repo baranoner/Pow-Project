b4501138022a322d2fb39d7057c93f703d5a4dba
Baran Öner <baranoner7@protonmail.com>
2025-10-27 17:06:14 +0300

    Polish for Portfolio

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 import express from "express"
 import bodyParser from "body-parser"
 import axios from "axios"
+import dotenv from "dotenv"
 
 
+dotenv.config();
 const app = express();
 const port = 3000;
-const API_KEY = "1a782afd5e4090ae5aed23a8e0309967";
+const API_KEY = process.env.API_KEY;
+
+var error = null;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("home.ejs");
+    res.render("home.ejs", {
+      error: error,
+    });
 })
 
 app.post("/submit", async (req, res) => {
   try {
-    console.log(req.body);
-    const heroName = req.body.heroName;
+    const heroName = req.body.heroName.trim().toLowerCase();
     const response = await axios.get(
       `https://superheroapi.com/api/${API_KEY}/search/${heroName}`
     );
     const result = response.data;
-    console.log(result);
     if(result.response === "success")
     {
         res.render("info.ejs", {
@@ -30,15 +34,13 @@ app.post("/submit", async (req, res) => {
         });
     }
     else{
-        res.render("home.ejs", {
-        error: result.error,
-    });
+        error = result.error;
+        res.redirect("/");
     }
   } catch (error) {
     console.error("Failed to make request:", error.message);
-    res.render("home.ejs", {
-      error: "No activities that match your criteria.",
-    });
+    error = error.message;
+    res.redirect("/");
   }
 });
 
